bd99bb44a027227b7d28ecc993d0654214dee4fc
Dmitry Kunin <dkunin@avito.ru>
2016-10-26 09:09:34 +0300

    master:Added some

--- key-mapper.js
+++ key-mapper.js
@@ -2,6 +2,7 @@
 
 module.exports = function(adresses) {
     var procEnv = process.env;
+    console.log(adresses);
     return adresses
         .map(singleKey => {
             return Object.keys(procEnv).find(singleEnvKey => procEnv[singleEnvKey] === singleKey);
